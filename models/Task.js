cf97fffd51f12990fd36a1e0cf51f31c4ce47c4d
Henrique Guazzelli Mendes <henrique.gmendes@hotmail.com>
2020-03-24 22:20:51 -0300

    feat(routes) implement get route to deliver create-task form | implement post route to save new task in db | implement home route to list all tasks created

--- models/Task.js
+++ models/Task.js
@@ -5,9 +5,9 @@ const taskSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
   status: { type: String, required: true, enum: ['TODO', 'ONGOING', 'DONE', 'CANCELED'] },
-  creationDate: { type: Date, required: true },
+  creationDate: { type: Date, required: true, default: new Date() },
   dueDate: { type: Date, required: true },
-  user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
+  user: { type: mongoose.Types.ObjectId, ref: 'User', required: false },
 });
 
 const Task = mongoose.model('Task', taskSchema);
